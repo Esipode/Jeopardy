21a5df5eb63c9acd5e85c0b340c750eca85832ae
Esipode <chad@chaddyck.com>
2025-03-29 14:25:23 -0600

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Game from "./pages/Game";
 
 import "./styles/global.scss";
