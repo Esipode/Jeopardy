2f45ed88e3cee09b577390e1c3ea064066978de8
Esipode <chad@chaddyck.com>
2025-03-29 12:42:02 -0600

    Remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Game from "./pages/Game";
 
 import "./styles/global.scss";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Game />} />
-      </Routes>
-    </Router>
-  );
+  return <Game />;
 };
 
 export default App;
